67ce7a26804ab3eab6a5efadda22837272068e82
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-01-30 16:54:19 -0300

    Enable stringing expressions
    Fixes the previous commit's bug that made impossible to string
    expressions one after the other by changing how the digit presses and
    the results are handled between the number variables
    
    Also got rid of variables that were not needed to keep the code
    a little cleaner

--- scripts.js
+++ scripts.js
@@ -19,26 +19,18 @@ function operate(n1, operator, n2) {
     switch (operator) {
         case "+":
             return add(n1, n2);
-            
+
         case "-":
             return subtract(n1, n2);
-           
+
         case "*":
             return multiply(n1, n2);
-            
+
         case "/":
             return divide(n1, n2);
-            
+
     }
 }
-
-
-// const allButtons = document.querySelectorAll('button');
-// allButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         console.log(button.textContent);
-//     });
-// });
 
 const display = document.querySelector('.display');
 const allButtons = document.querySelectorAll('button');
@@ -49,51 +41,47 @@ allButtons.forEach(function (button) {
 function buttonClick() {
     switch (this.className) {
         case "digit":
-            numberInput += this.textContent;
-            display.textContent = numberInput;
+            if (!operator) {
+                num1 += this.textContent;
+                display.textContent = num1;
+            } else {
+                num2 += this.textContent;
+                display.textContent = num2;
+            }
             break;
         case "operator":
-            if(!operator){
-            num2 = num1;
+            if (operator) {
+                result = operate(num1, operator, num2);
+                num1 = result;
+                display.textContent = result;
+                refreshKeepResult();
             }
-            else {
-            partial = operate(num1,operator,num2);
-            num2 = partial;
-            }
-            num1 = numberInput;
-            numberInput = '';
             operator = this.textContent;
-            display.textContent = operator;
             break;
         case "equal":
-            if (!num2 && numberInput) {
-                num2 = numberInput;
-            }
-            result = operate(num1,operator,num2);
+            result = operate(num1, operator, num2);
             display.textContent = result;
+            refreshKeepResult();
             break;
         case "clear":
             display.textContent = '';
             num1 = '';
             num2 = '';
-            numberInput = '';
             operator = '';
-            partial = '';
             result = '';
             break;
     }
 
 }
 
+function refreshKeepResult() {
+    num1 = result;
+    num2 = '';
+    operator = '';
+    result = '';
+}
 
+let operator = '';
+let result = '';
 let num1 = '';
 let num2 = '';
-let numberInput = '';
-let operator = '';
-let partial = '';
-let result = '';
-
-// operate(2,"+",0);
-// operate(2,"-",0);
-// operate(2,"*",0);
-// operate(2,"/",0);
